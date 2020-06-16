ed37cb8c85d14f9bd6779a5e8a52c0751b43c742
Mojelloul <jelloulmohamed11@gmail.com>
2020-06-16 20:42:17 +0200

    commentaire

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-
+//modifier
 const port = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
